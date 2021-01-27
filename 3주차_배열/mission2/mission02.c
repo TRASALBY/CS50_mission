eb89fb3a1f843f412ee2140477d55a4e1b4feddd
TRASALBY <ghtjd8264@naver.com>
2021-01-27 17:42:06 +0900

    mission02 업로드
    미션 02 해결, 1부터 N까지의 수중 빠진숫자 찾기
    주어진 모든수를 더해서 1부터 N까지의 합에서 빼면 빠진 숫자를 찾을수 있다.

--- 3주차_배열/mission2/mission02.c
+++ 3주차_배열/mission2/mission02.c
@@ -1,4 +1,25 @@
-#include    <stdio.h>
-int main(void){
-    
+#include <stdio.h>
+#define SIZE 500000
+
+int main(int arge, char*argv[]) {
+    int n;
+    int sum = 0;
+    int k = 0;
+    scanf("%d", &n);
+
+    // 1부터 N의 숫자중 K가 빠진 배열
+    int partArr[SIZE];
+    int lengthOfPartArr = n-1;
+
+    for(int i=0; i < lengthOfPartArr; i++){
+        scanf("%d", &partArr[i]);
+              sum += partArr[i];
+    }
+
+    for(int i = 1; i<=n;i++){
+        k+=i;
+    }
+    k = k - sum;
+    printf("k = %i\n", k);
+    return 0;
 }
